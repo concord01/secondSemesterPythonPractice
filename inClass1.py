7a5672f6908a4f0f1cc8f8b7c22e3bb68df80541
Joaquin Hoyem <joaquin.hoyem@gmail.com>
2025-02-24 19:12:21 +0000

    name updates and challenge 3 completed.

---
+++ inClass1.py
@@ -0,0 +1,22 @@
+# #math vars
+# a = 100
+# b = 9
+
+# c = 9 * a
+# d = b / 3
+
+# d = int(d)
+
+# print(c)
+# print(d)
+
+# #asking user for the number then multplying by 2
+# num1 = input("what number do you want?")
+# num1 = int(num1) #converting string to int
+# print(num1*2)
+
+#program that prints out a first and last name
+first_name = input("What's your first name?")
+last_name = input("What's your last name?")
+
+print(first_name + " " + last_name)
